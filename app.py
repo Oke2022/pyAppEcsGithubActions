f26218cfd7d656de1960e7b1ff2822021c864070
Oke2022 <xpdigita@gmail.com>
2025-04-23 16:53:36 -0400

    fixing bug

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/health', methods=['GET'])
-def health_check():
-    return render_template('health.html')
+# @app.route('/health', methods=['GET'])
+# def health_check():
+#     return render_template('health.html')
+@app.route('/health')
+def health():
+    return 'OK', 200
 
 @app.route('/')
 def home():
